8c9dabf618849b230b7ab7a94d20b092fb23f306
Bijay <Bijaym095@gmail.com>
2023-02-02 09:56:08 +0545

    Data stored in local storage and README.md updated

--- src/context/Expensetracker/ExpenseTrackerState.js
+++ src/context/Expensetracker/ExpenseTrackerState.js
@@ -1,8 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ExpenseTrackerContext from "./expenseTrackerContext";
 
 const ExpenseTrackerState = ({ children }) => {
   const [formData, setFormData] = useState([]);
+
+  //fetching data from local storage
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("formData"));
+    if (storedData) {
+      setFormData(storedData);
+    }
+  }, []);
+
+  //stored data in local storage
+  useEffect(() => {
+    localStorage.setItem("formData", JSON.stringify(formData));
+  }, [formData]);
 
   return (
     <ExpenseTrackerContext.Provider value={{ formData, setFormData }}>
